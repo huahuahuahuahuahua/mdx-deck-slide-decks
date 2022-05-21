0ff177bc3edd5f731589af0040b130c796075b43
huawink <1017739427@qq.com>
2022-05-21 23:23:14 +0800

    feat:初始化演示项目，添加deck.mdx

--- src/greeting-loader.js
+++ src/greeting-loader.js
@@ -1,38 +1,38 @@
-// import React from "react";
+import React from "react";
 
-// import { useThemeUI } from "theme-ui";
+import { useThemeUI } from "theme-ui";
 
-// const load = require.context("./", true, /greeting\..*js$/);
+const load = require.context("./", true, /greeting\..*js$/);
 
-// export default function GreetingLoader({
-//   version = "1.1",
-//   theme,
-// }) {
-//   const Greeting = load(
-//     "./greeting." + version + ".js"
-//   ).default;
+export default function GreetingLoader({
+  version = "1.1",
+  theme,
+}) {
+  const Greeting = load(
+    "./greeting." + version + ".js"
+  ).default;
 
-//   const { themeUi } = useThemeUI();
+  const { themeUi } = useThemeUI();
 
-//   const t = theme || themeUi;
+  const t = theme || themeUi;
 
-//   if (typeof window == `undefined`) {
-//     return null;
-//   }
+  if (typeof window == `undefined`) {
+    return null;
+  }
 
-//   return (
-//     <div
-//       style={{
-//         height: "100%",
-//         width: "100%",
-//         display: "flex",
-//         justifyContent: "center",
-//         alignItems: "center",
-//         background:
-//           t && t.styles.CodeSurfer.code.backgroundColor,
-//       }}
-//     >
-//       <Greeting name="Mary" />
-//     </div>
-//   );
-// }
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background:
+          t && t.styles.CodeSurfer.code.backgroundColor,
+      }}
+    >
+      <Greeting name="Mary" />
+    </div>
+  );
+}
